976fc1d6f7165746cb13a095277b2b0e57b6f8e6
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-17 23:45:22 -0500

    Create fmt_xxx.h placeholders

---
+++ source/include/fmt_mt.h
@@ -0,0 +1,17 @@
+#ifndef __ZOE3_FMT_MT_H__
+#define __ZOE3_FMT_MT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+//
+//
+//
+//
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
