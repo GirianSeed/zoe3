fdae400f085b3a492d0274c2f49fa1d006efde01
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-03-06 14:01:10 -0500

    add chara strcode defines

--- source/include/strcode.h
+++ source/include/strcode.h
@@ -24,10 +24,51 @@
 
 /*---------------------------------------------------------------------------*/
 
-//
-//
-//
-//
-//
+#define CHARA_00011bc5          0x00011bc5
+#define CHARA_000e5e72          0x000e5e72
+#define CHARA_0016257c          0x0016257c
+#define CHARA_00198c91          0x00198c91
+#define CHARA_001ae209          0x001ae209
+#define CHARA_0024dff0          0x0024dff0
+#define CHARA_002ec13f          0x002ec13f
+#define CHARA_0039e843          0x0039e843
+#define CHARA_003cba7d          0x003cba7d
+#define CHARA_005a1cbc          0x005a1cbc
+#define CHARA_005a8c59          0x005a8c59
+#define CHARA_00635875          0x00635875
+#define CHARA_006856c1          0x006856c1
+#define CHARA_006d2891          0x006d2891
+#define CHARA_00743c9f          0x00743c9f
+#define CHARA_007f39b4          0x007f39b4
+#define CHARA_0080b977          0x0080b977
+#define CHARA_0082c4fc          0x0082c4fc
+#define CHARA_0087df1a          0x0087df1a
+#define CHARA_00899c97          0x00899c97
+#define CHARA_0089c493          0x0089c493
+#define CHARA_0089f7ed          0x0089f7ed
+#define CHARA_0089fdf9          0x0089fdf9
+#define CHARA_008c4700          0x008c4700
+#define CHARA_008dadc5          0x008dadc5
+#define CHARA_00944e11          0x00944e11
+#define CHARA_009936ee          0x009936ee
+#define CHARA_00993746          0x00993746
+#define CHARA_009f0c4d          0x009f0c4d
+#define CHARA_00a53aa7          0x00a53aa7
+#define CHARA_00a7f442          0x00a7f442
+#define CHARA_00ab5351          0x00ab5351
+#define CHARA_00b8dcee          0x00b8dcee
+#define CHARA_00bac5db          0x00bac5db
+#define CHARA_00c8e3fd          0x00c8e3fd
+#define CHARA_00c8fc31          0x00c8fc31
+#define CHARA_00c91ea7          0x00c91ea7
+#define CHARA_00cacd28          0x00cacd28
+#define CHARA_00daf423          0x00daf423
+#define CHARA_00dc83c5          0x00dc83c5
+#define CHARA_00dec222          0x00dec222
+#define CHARA_00ea1f40          0x00ea1f40
+#define CHARA_00f07377          0x00f07377
+#define CHARA_00f3d072          0x00f3d072
+#define CHARA_00f95b53          0x00f95b53
+#define CHARA_00fbeb7f          0x00fbeb7f
 
 #endif // {{{ END OF FILE }}}
