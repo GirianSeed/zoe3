0fe99a399d2310919699e41ce84616e17af9c096
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-03-06 14:00:40 -0500

    align strcode.h defines

--- source/include/strcode.h
+++ source/include/strcode.h
@@ -3,22 +3,24 @@
 
 /*---------------------------------------------------------------------------*/
 
-#define GCX_scenerio    0x00180720  // GV_StrCode("scenerio")
+#define GCX_scenerio            0x00180720  // GV_StrCode("scenerio")
 
-#define CMD_if          0x00000d86  // GV_StrCode("if")
-#define CMD_switch      0x00a65db5  // GV_StrCode("switch")
-#define CMD_eval        0x0034648c  // GV_StrCode("eval")
-#define CMD_call        0x003311ec  // GV_StrCode("call")
-#define CMD_return      0x008be398  // GV_StrCode("return")
-#define CMD_print       0x003ab23b  // GV_StrCode("print")
+/* libgcl/basic.c */
+#define CMD_if                  0x00000d86  // GV_StrCode("if")
+#define CMD_switch              0x00a65db5  // GV_StrCode("switch")
+#define CMD_eval                0x0034648c  // GV_StrCode("eval")
+#define CMD_call                0x003311ec  // GV_StrCode("call")
+#define CMD_return              0x008be398  // GV_StrCode("return")
+#define CMD_print               0x003ab23b  // GV_StrCode("print")
 
-#define CMD_load        0x0037c884  // GV_StrCode("load")
-#define CMD_preseek     0x008b3df5  // GV_StrCode("preseek")
-#define CMD_command     0x00082bc9  // GV_StrCode("command")
-#define CMD_chara       0x006592a7  // GV_StrCode("chara")
-#define CMD_mesg        0x003822c7  // GV_StrCode("mesg")
-#define CMD_restart     0x006bb005  // GV_StrCode("restart")
-#define CMD_trap        0x003bd490  // GV_StrCode("trap")
+/* game/script.c */
+#define CMD_load                0x0037c884  // GV_StrCode("load")
+#define CMD_preseek             0x008b3df5  // GV_StrCode("preseek")
+#define CMD_command             0x00082bc9  // GV_StrCode("command")
+#define CMD_chara               0x006592a7  // GV_StrCode("chara")
+#define CMD_mesg                0x003822c7  // GV_StrCode("mesg")
+#define CMD_restart             0x006bb005  // GV_StrCode("restart")
+#define CMD_trap                0x003bd490  // GV_StrCode("trap")
 
 /*---------------------------------------------------------------------------*/
 
