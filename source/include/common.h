dabba05e3305462e0988033d6f056766f7891cbb
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-07 03:36:57 -0400

    add RGBA32 macros to common.h

--- source/include/common.h
+++ source/include/common.h
@@ -81,6 +81,73 @@
     }
 
 /*---------------------------------------------------------------------------*/
+
+/* RGBA8888 format */
+#ifdef WORDS_BIGENDIAN
+#define RGBA_R_SHIFT    (24)
+#define RGBA_G_SHIFT    (16)
+#define RGBA_B_SHIFT    ( 8)
+#define RGBA_A_SHIFT    ( 0)
+#else
+#define RGBA_R_SHIFT    ( 0)
+#define RGBA_G_SHIFT    ( 8)
+#define RGBA_B_SHIFT    (16)
+#define RGBA_A_SHIFT    (24)
+#endif
+
+#define MAKE_RGBA(_r,_g,_b,_a)                                  \
+        ((unsigned int)((((_r) & 0xff) << RGBA_R_SHIFT)|        \
+                        (((_g) & 0xff) << RGBA_G_SHIFT)|        \
+                        (((_b) & 0xff) << RGBA_B_SHIFT)|        \
+                        (((_a) & 0xff) << RGBA_A_SHIFT)))
+
+#define MAKE_RGB0(_r,_g,_b)     MAKE_RGBA(_r,_g,_b,0x00)
+#define MAKE_RGBX(_r,_g,_b)     MAKE_RGBA(_r,_g,_b,0xff)
+
+#define GET_R_FROM_RGBA(_rgba)  (((_rgba) >> RGBA_R_SHIFT) & 0xff)
+#define GET_G_FROM_RGBA(_rgba)  (((_rgba) >> RGBA_G_SHIFT) & 0xff)
+#define GET_B_FROM_RGBA(_rgba)  (((_rgba) >> RGBA_B_SHIFT) & 0xff)
+#define GET_A_FROM_RGBA(_rgba)  (((_rgba) >> RGBA_A_SHIFT) & 0xff)
+
+/* ARGB8888 format */
+#ifdef WORDS_BIGENDIAN
+#define ARGB_A_SHIFT    (24)
+#define ARGB_R_SHIFT    (16)
+#define ARGB_G_SHIFT    ( 8)
+#define ARGB_B_SHIFT    ( 0)
+#else
+#define ARGB_A_SHIFT    ( 0)
+#define ARGB_R_SHIFT    ( 8)
+#define ARGB_G_SHIFT    (16)
+#define ARGB_B_SHIFT    (24)
+#endif
+
+#define MAKE_ARGB(_a,_r,_g,_b)                                  \
+        ((unsigned int)((((_a) & 0xff) << ARGB_A_SHIFT)|        \
+                        (((_r) & 0xff) << ARGB_R_SHIFT)|        \
+                        (((_g) & 0xff) << ARGB_G_SHIFT)|        \
+                        (((_b) & 0xff) << ARGB_B_SHIFT))
+
+#define MAKE_0RGB(_r,_g,_b)     MAKE_ARGB(0x00,_r,_g,_b)
+#define MAKE_XRGB(_r,_g,_b)     MAKE_ARGB(0xff,_r,_g,_b)
+
+#define GET_A_FROM_ARGB(_argb)  (((_argb) >> ARGB_A_SHIFT) & 0xff)
+#define GET_R_FROM_ARGB(_argb)  (((_argb) >> ARGB_R_SHIFT) & 0xff)
+#define GET_G_FROM_ARGB(_argb)  (((_argb) >> ARGB_G_SHIFT) & 0xff)
+#define GET_B_FROM_ARGB(_argb)  (((_argb) >> ARGB_B_SHIFT) & 0xff)
+
+/* common colors */
+#define COLOR_BLACK     MAKE_RGB0(  0,  0,  0)
+#define COLOR_WHITE     MAKE_RGB0(255,255,255)
+#define COLOR_GRAY      MAKE_RGB0(128,128,128)
+#define COLOR_RED       MAKE_RGB0(255,  0,  0)
+#define COLOR_GREEN     MAKE_RGB0(  0,255,  0)
+#define COLOR_BLUE      MAKE_RGB0(  0,  0,255)
+#define COLOR_CYAN      MAKE_RGB0(  0,255,255)
+#define COLOR_MAGENTA   MAKE_RGB0(255,   ,255)
+#define COLOR_YELLOW    MAKE_RGB0(255,255,  0)
+
+/*---------------------------------------------------------------------------*/
 // https://en.cppreference.com/w/c/language/arithmetic_types#Boolean_type
 // NOTE: (bool)0.5 evaluates to true, whereas (int)0.5 evaluates to 0.
 
