c43c2cb8c1077e9f1de3d5c32773693d15f4aedc
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-27 01:49:38 -0400

    Fix broken magenta color macro

--- source/include/mgs_defs.h
+++ source/include/mgs_defs.h
@@ -147,7 +147,7 @@
 #define COLOR_GREEN     MAKE_RGB0(  0,255,  0)
 #define COLOR_BLUE      MAKE_RGB0(  0,  0,255)
 #define COLOR_CYAN      MAKE_RGB0(  0,255,255)
-#define COLOR_MAGENTA   MAKE_RGB0(255,   ,255)
+#define COLOR_MAGENTA   MAKE_RGB0(255,  0,255)
 #define COLOR_YELLOW    MAKE_RGB0(255,255,  0)
 
 #endif // {{{ END OF FILE }}}
