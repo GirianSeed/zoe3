580aff3897cf08eb235d5465df7201f5bdf87279
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-21 11:15:41 -0400

    Update chara.h

--- source/include/chara.h
+++ source/include/chara.h
@@ -6,11 +6,11 @@
 // XXX The empty parameter list is widely regarded as one of old C's
 // XXX anti-features. C++ and C23 have changed its meaning to (void).
 
-typedef void *(*NEWCHARA)();
+typedef void *NEWCHARA();
 
 typedef struct {
     unsigned int class_id;
-    NEWCHARA func;
+    NEWCHARA *func;
 } CHARA;
 
 #endif // {{{ END OF FILE }}}
