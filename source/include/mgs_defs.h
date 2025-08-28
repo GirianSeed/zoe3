ac59669bee9247291ea3f84ace18ef934d14b0c3
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-27 23:12:17 -0400

    add note about ee-gcc's wrong LONG_MIN/MAX values
    Changed synched with MGS3.

--- source/include/mgs_defs.h
+++ source/include/mgs_defs.h
@@ -4,6 +4,10 @@
 #include <stddef.h>     // for NULL
 #include <limits.h>
 
+// NOTE: ee-gcc 2.9/2.96's limits.h wrongly defines the minimum and maximum
+// values of 'long int' as if it were 32-bits wide, when it's actually 64-bits.
+// This problem was fixed with ee-gcc 3.2.
+
 /* MSVC defines _countof as an extension to stdlib.h */
 #ifndef _countof
 #define _countof(array) (sizeof(array)/sizeof(array[0]))
