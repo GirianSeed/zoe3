db4a4d1132bf946345c0c226440ee3c8676d99aa
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-09-28 11:40:10 -0400

    don't include strcode.h via chara.h

--- source/include/chara.h
+++ source/include/chara.h
@@ -1,8 +1,6 @@
 #ifndef __ZOE3_CHARA_H__
 #define __ZOE3_CHARA_H__
 
-#include "strcode.h"
-
 // XXX The empty parameter list is widely regarded as one of old C's
 // XXX anti-features. C++ and C23 have changed its meaning to (void).
 
